f18bdeeafe312799ce04b6809469ab0418557b3d
KalelAufa <163893546+KalelAufa@users.noreply.github.com>
2024-09-24 07:20:31 +0700

    Add files via upload

---
+++ PHP/lat01.php
@@ -0,0 +1,12 @@
+<?php 
+
+    echo "Belajar PHP";
+    echo "<br>";
+    echo "Saya Siswa SMK";
+    echo '<h1 style="background-color: red;">Belajar PHP itu Mudah</h1>
+            <p style="background-color: blue;">saya suka belajar program</p>
+    ';
+    echo 'saya belajar'.' PHP'.'<br>';
+    echo 2019 + 2;
+
+?>
