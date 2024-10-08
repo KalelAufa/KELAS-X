249fe1dfdb3e6fdbb87cd798bd233907d4404483
KalelAufa <mkalelaufa@gmail.com>
2024-10-08 20:46:58 +0700

    Materi

---
+++ CPP/2024-10-07/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    cout << "Perulangan for dengan i--" << endl;
+
+    // for (int i = 50; i >= 1; i--)
+    // {
+    //     cout << i  << " ";
+    // }
+
+    //GENAP
+    
+    // for (int i = 50; i >= 1; i--)
+    // {
+    //     cout << (i * 2) <<  " ";
+    // }
+
+    //GANJIL
+
+    // for (int i = 50; i >= 1; i--)
+    // {
+    //     cout << (i * 2) - 1 <<  " ";
+    // }
+
+    // for (int i = 100; i >= 0; i--)
+    // {
+    //     if ( i % 2 == 0)
+    //     {
+    //         cout << i << " ";
+    //     }
+    // }
+
+    // for (int i = 100; i >= 1; i--)
+    // {
+    //     if (i % 2 != 0)
+    //     {
+    //         cout << i << " ";
+    //     }
+    // }
+
+    // for (int i = 30; i >= 0; i--)
+    // {
+    //     if ( i % 3 == 0)
+    //     {
+    //         cout << i << "  ";
+    //     }
+    // }
+
+    // for (int i = 15; i >= 0; i--)
+    // {
+    //         cout << i << endl;
+    // }
+
+    // int array[100];
+
+    // for (int i = 100; i >= 0; i--)
+    // {
+    //     array[i] = i ;
+    // }
+
+    // for (int i = 100; i >= 0; i--)
+    // {
+    //     std::cout << "Array [" << i << "] = " << array[i] << std::endl;
+    // }
+
+    // int array[100];
+
+    // for (int i = 50; i >= 0; i--)
+    // {
+    //     array[i] = i*2;
+    // }
+
+    // for (int i = 50; i >= 0; i--)
+    // {
+    //     std::cout << "Array [" << i*2 << "] = " << array[i] << std::endl;
+    // }
+
+    // int array[100];
+
+    // for (int i = 50; i >= 0; i--)
+    // {
+    //     array[i] = (i * 2) -1;
+    // }
+
+    // for (int i = 50; i >= 0; i--)
+    // {
+    //     std::cout << "Array [" << (i * 2) - 1 << "] = " << array[i] << std::endl;
+    // }
+
+    return 0;
+}
