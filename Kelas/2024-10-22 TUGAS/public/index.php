01039ef7df14bfe0264ce22792c4db05865a0206
KalelAufa <mkalelaufa@gmail.com>
2024-10-25 19:27:04 +0700

    tugas

---
+++ Kelas/2024-10-22 TUGAS/public/index.php
@@ -0,0 +1,128 @@
+<?php 
+    $identitas = ["M. KAL'EL AKBAR AUFA P. E. Z.", "Omah Pesona Buduran", "PELAJAR", "081235500150", "mkalelaufa@gmail.com"];
+    $skill = ["HTML", "CSS", "PHP", "C++"];
+    $sekolah = ["MI DARUL HIKMAH ENTALSEWU", "SDN PAGERWOJO", "SMPN 1 SIDOARJO",  "SMKN 2 BUDURAN"];
+    $main = ["PROFIL", "EDUKASI", "TELEPON", "EMAIL", "ALAMAT", "SKILL", "HOBI"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../src/output.css">
+    <title>TUGAS CURRICULUM VITAE</title>
+    <style>
+        @font-face {
+            font-family: intro-black;
+            src: url(../fonts/Intro/Intro\ Black\ Alt.otf);
+        }
+        @font-face {
+            font-family: intro-bold;
+            src: url(../fonts/Intro/Intro\ Bold\ Alt.otf);
+        }
+        @font-face {
+            font-family: intro-light;
+            src: url(../fonts/Intro/Intro\ Light\ Alt.otf);
+        }
+    </style>
+</head>
+<body class="bg-slate-500">
+    <div class="flex items-center justify-center p-4">
+        <div class="w-full max-w-3xl bg-white shadow-lg rounded-xl">
+            <div class="flex flex-col md:flex-row">
+                <div class="flex items-center justify-center p-4 md:w-1/3">
+                    <img src="../image/pp.jpeg" alt="foto profile" class="w-24 h-24 rounded-full md:w-32 md:h-32">
+                </div>
+                <div class="flex flex-col items-center justify-center p-4 md:w-2/3 rounded-r-xl">
+                    <h1 class="text-xl font-bold text-center md:text-left" style="font-family:'intro-black';";><?=  $identitas[0] ?></h1>
+                    <p class="text-sm text-center md:text-left" style="font-family:'intro-bold';"><?=   $identitas[2] ?></p>
+                </div>
+            </div>
+            <div class="flex flex-col md:flex-row">
+                <div class="p-4 md:w-1/2">
+                    <div class="mb-4">
+                        <h1 class="font-bold" style="font-family: 'intro-bold';"><?= $main[0] ?></h1>
+                        <p style="font-family: 'intro-light';">
+                            Saya adalah seorang pelajar berusia 16 tahun yang bersemangat dalam dunia teknologi dan pemrograman. 
+                            Dengan rasa ingin tahu yang tinggi dan keinginan untuk terus belajar, saya selalu berusaha mengembangkan 
+                            keterampilan dalam berbagai bahasa pemrograman dan teknologi web. Meskipun masih muda, saya memiliki 
+                            tekad kuat untuk menjadi seorang pengembang web yang handal di masa depan.
+                        </p>
+                    </div>
+                    <div>
+                        <div>
+                            <h1 class="font-bold" style="font-family: 'intro-bold';"><?= $main[1] ?></h1>
+                            <ul class="ml-5 list-disc" style="font-family: 'intro-light';">
+                                <?php foreach($sekolah as $school): ?>
+                                    <li><?= $school ?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-4 md:w-1/2">
+                    <div class="mb-4">
+                        <h1 class="font-bold" style="font-family: 'intro-bold';"><?= $main[5] ?></h1>
+                        <div class="ml-3">
+                            <p style="font-family: 'intro-light';">HTML</p>
+                            <div class="w-full bg-gray-300 rounded-full">
+                                <div class="text-center text-white bg-blue-600 rounded-full" style="width:75%;">75%</div>
+                            </div>
+                            <p style="font-family: 'intro-light';">CSS</p>
+                            <div class="w-full bg-gray-300 rounded-full">
+                                <div class="text-center text-white bg-blue-600 rounded-full" style="width:50%;">50%</div>
+                            </div>
+                            <p style="font-family: 'intro-light';">PHP</p>
+                            <div class="w-full bg-gray-300 rounded-full">
+                                <div class="text-center text-white bg-blue-600 rounded-full" style="width:45%;">45%</div>
+                            </div>
+                            <p style="font-family: 'intro-light';">C++</p>
+                            <div class="w-full bg-gray-300 rounded-full">
+                                <div class="text-center text-white bg-blue-600 rounded-full" style="width:55%;">55%</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h1 class="font-bold" style="font-family: 'intro-bold';"><?= $main[6] ?></h1>
+                        <ul class="ml-5 list-disc" style="font-family: 'intro-light';">
+                            <li>Belajar  Bahasa Pemrograman</li>
+                            <li>Badminton</li>
+                            <li>Mendengarkan Musik</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="p-4 md:flex md:flex-row md:gap-10">
+                <div class="flex items-center gap-2 mb-2">
+                    <div class="mr-2">
+                        <img src="../image/telpon.png" alt="logo" class="w-6 h-6 scale-110">
+                    </div>
+                    <div>
+                        <h1 class="font-bold" style="font-family: 'intro-bold';"><?= $main[2] ?></h1>
+                        <p style="font-family: 'intro-light';"><?= $identitas[3] ?></p>
+                    </div>
+                </div>
+                <div class="flex items-center gap-2 mb-2">
+                    <div class="mr-2">
+                        <img src="../image/lokasi.png" alt="logo" class="w-6 h-6 scale-125">
+                    </div>
+                    <div>
+                        <h1 class="font-bold" style="font-family: 'intro-bold';"><?= $main[3] ?></h1>
+                        <p style="font-family: 'intro-light';"><?= $identitas[1] ?></p>
+                    </div>
+                </div>
+                <div class="flex items-center gap-3 mb-2">
+                    <div class="mr-2">
+                        <img src="../image/email.png" alt="logo" class="w-6 h-6 scale-150">
+                    </div>
+                    <div>
+                        <h1 class="font-bold" style="font-family: 'intro-bold';"><?= $main[4] ?></h1>
+                        <p style="font-family: 'intro-light';"><?= $identitas[4] ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ Kelas/2024-10-22 TUGAS/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{php,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
