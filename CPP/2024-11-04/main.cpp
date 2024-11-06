2ee7525f65d9b71a9f7923a6910bf55223d88b1a
KalelAufa <mkalelaufa@gmail.com>
2024-11-06 09:27:54 +0700

    tugas

--- CPP/2024-11-04/main.cpp
+++ CPP/2024-11-04/main.cpp
@@ -6,38 +6,41 @@ int main()
 {
     string namaLengkap, namaPanggilan, namaPacar, jurusan, sekolah, alamat, hobi, citaCita;
 
-    cout << "Nama Lengkap : ";
-    getline(cin, namaLengkap);
-    cout << "Nama Panggilan : ";
-    getline(cin, namaPanggilan);
-    cout << "Nama Pacar : ";
-    getline(cin, namaPacar);
-    cout << "Jurusan : ";
-    getline(cin, jurusan);
-    cout << "Sekolah : ";
-    getline(cin, sekolah);
-    cout << "Alamat : ";
-    getline(cin, alamat);
-    cout << "Hobi : ";
-    getline(cin, hobi);
-    cout << "Cita-Cita : ";
-    getline(cin, citaCita);
-
-    string identitas[8] = {
-        "Nama Lengkap : " + namaLengkap,
-        "Nama Panggilan : " + namaPanggilan,
-        "Nama Pacar : " + namaPacar,
-        "Jurusan : " + jurusan,
-        "Sekolah : " + sekolah,
-        "Alamat : " + alamat,
-        "Hobi : " + hobi,
-        "Cita-Cita : " + citaCita};
-
-    cout << "\nIdentitas : " << endl;
-    for ( string i : identitas)
+    for (int j = 1; j <= 2; j++)
     {
-        cout << i << endl;
-    }
+        cout << "\nData Siswa ke " << j << endl;
+        cout << "Nama Lengkap : ";
+        getline(cin, namaLengkap);
+        cout << "Nama Panggilan : ";
+        getline(cin, namaPanggilan);
+        cout << "Nama Pacar : ";
+        getline(cin, namaPacar);
+        cout << "Jurusan : ";
+        getline(cin, jurusan);
+        cout << "Sekolah : ";
+        getline(cin, sekolah);
+        cout << "Alamat : ";
+        getline(cin, alamat);
+        cout << "Hobi : ";
+        getline(cin, hobi);
+        cout << "Cita-Cita : ";
+        getline(cin, citaCita);
 
+        string identitas[8] = {
+            "Nama Lengkap : " + namaLengkap,
+            "Nama Panggilan : " + namaPanggilan,
+            "Nama Pacar : " + namaPacar,
+            "Jurusan : " + jurusan,
+            "Sekolah : " + sekolah,
+            "Alamat : " + alamat,
+            "Hobi : " + hobi,
+            "Cita-Cita : " + citaCita};
+
+        cout << "\nIdentitas Pelajar: " << endl;
+        for (string i : identitas)
+        {
+            cout << i << endl;
+        }
+    }
     return 0;
 }
