2d969c0e40d8e1ddec91b10b0d780b78e798fda4
KalelAufa <mkalelaufa@gmail.com>
2025-01-09 19:42:46 +0700

    .

---
+++ JAVA/BelajarJavaDasar/src/project1.java
@@ -0,0 +1,7 @@
+public class project1 {
+    public static void main(String[] args) {
+        System.out.println("KALIL");
+        System.out.println("X-RPL");
+        System.out.println("15");
+    }
+}
---
+++ JAVA/BelajarJavaDasar/src/project2.java
@@ -0,0 +1,6 @@
+public class project2 {
+    public static void main(String[] args) {
+        System.out.println("Saya Belajar JAVA");
+        System.out.println("Umur Saya "+17+" Tahun");
+    }
+}
